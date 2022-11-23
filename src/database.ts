18e0c7b0dbd7b39183cb7550042e5d2872ba06f8
Thiago <thiago.spereira02@gmail.com>
2022-11-23 20:43:56 -0300

    fixing database

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-    connectionString: process.env.DB_URI,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
