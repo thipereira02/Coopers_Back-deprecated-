358eabf92e32a9bce47bfb01e6600445664598bf
Thiago <thiago.spereira02@gmail.com>
2022-11-23 15:59:07 -0300

    changing database connection

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DB_URI,
     ssl: {
         rejectUnauthorized: false,
     },
